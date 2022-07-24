06d338b7045cb24b8b066d67144f278a332d8612
VanshikaGupta <105194105+Vanshiii234@users.noreply.github.com>
2022-07-24 01:24:14 -0500

    Update PRAC4.cpp

--- PRAC4.cpp
+++ PRAC4.cpp
@@ -1,218 +1,164 @@
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#define MAX_DIM 100
+#include<iostream>
 using namespace std;
 
-
-class Relation
+class relation
 {
-private:
-    int size;
-    int set[MAX_DIM];
-    int imageCount[MAX_DIM];
-    int matrix[MAX_DIM][MAX_DIM];
-    int images[MAX_DIM][MAX_DIM];
+	int** ar;
+	int n;
 
-protected:
-    void getSet();
-    void putSet();
-    void getRelation();
-    void putRelation();
-    int indexOf(int);
-
-public:
-    Relation();
-    void display();
-    bool isReflexive();
-    bool isSymmetric();
-    bool isAntiSymmetric();
-    bool isTransitive();
+	public:
+		void input();
+		bool reflexive();
+		bool symmetric();
+		bool antisymmetric();
+		bool transitive();
+		void display();
 };
 
-
-Relation::Relation()
+void relation::input()
 {
-    getSet();
-    getRelation();
+	cout<<"enter the size of set as an array : ";
+	cin>>n;
+	ar=new int*[n];
+	for(int i=0;i<n;i++)
+		ar[i]=new int[n];
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			ar[i][j]=0;
+		}
+	}
+	int m;
+	cout<<"Enter the no of relations you want:";
+	cin>>m;
+	int a[m],b[m];
+	cout<<"Enter the relation:";
+	for(int i=0;i<m;i++)
+		cin>>a[i]>>b[i];
+	for(int i=0;i<m;i++)
+		ar[(a[i]-1)][(b[i]-1)]=1;
 }
 
-
-void Relation::display()
+void relation::display()
 {
-    putSet();
-    putRelation();
+	cout<<"\nThe Relation Matrix is:\n";
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			cout<<ar[i][j]<<" ";
+		}
+		cout<<endl;
+	}
 }
 
-
-void Relation::getSet()
+bool relation::reflexive()
 {
-    printf("Enter Size of Set: ");
-    scanf("%i", &size);
-    if (size != 0)
-    {
-        printf("Enter Elements of Set: ");
-        for (int i = 0; i < size; i++)
-            scanf("%i", &set[i]);
-    }
+	int f=1;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(i==j && ar[i][j]!=1)
+			{
+				f=0;
+				break;
+			}
+		}
+		if(f==0)
+			break;
+	}
+	if(f==1)
+		return true;
+	else
+		return false;
 }
 
-
-void Relation::putSet()
+bool relation::symmetric()
 {
-    printf("Set: { ");
-    if (size != 0)
-    {
-        for (int i = 0; i < size; i++)
-            printf("%i, ", set[i]);
-        printf("\b\b }\n");
-    }
-    else
-    {
-        printf("}\n");
-    }
+	int f=1;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(ar[i][j]!=ar[j][i])
+			{
+				f=0;
+				break;
+			}
+		}
+		if(f==0)
+			break;
+	}
+	if(f==1)
+		return true;
+	else
+		return false;
 }
 
-
-int Relation::indexOf(int el)
+bool relation::transitive()
 {
-    int i = 0;
-    while (i < size)
-    {
-        if (set[i] == el)
-            return i;
-        i++;
-    }
-    return -1;
+	int f=1;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(ar[i][j]==1)
+			{
+				for(int x=0;x<n;x++)
+				{
+					if(ar[j][x]==1 && ar[i][x]!=1)
+					{
+						f=0;
+						break;
+					}
+				}
+			}
+			if(f==0)
+				break;
+		}
+		if(f==0)
+			break;
+	}
+	if(f==1)
+		return true;
+	else
+		return false;
 }
 
-
-void Relation::getRelation()
+bool relation::antisymmetric()
 {
-    for (int i = 0; i < size; i++)
-    {
-        printf("Enter number of images for Set Element %i: ", set[i]);
-        scanf("%i", &imageCount[i]);
-        if (imageCount[i] != 0)
-        {
-            printf("Enter images of Set Element %i: ", set[i]);
-            for (int j = 0; j < imageCount[i]; j++)
-            {
-                scanf("%i", &images[i][j]);
-                if (indexOf(images[i][j]) == -1)
-                {
-                    printf("Error: Image not in Set\n");
-                    i--;
-                    break;
-                }
-                else
-                {
-                    matrix[i][indexOf(images[i][j])] = 1;
-                }
-            }
-        }
-    }
-    printf("\n");
-}
-
-
-void Relation::putRelation()
-{
-    printf("Relation R:\n{ ");
-    if (size != 0)
-    {
-        for (int i = 0; i < size; i++)
-        {
-            if (imageCount[i] != 0)
-                printf("\n ");
-            {
-                for (int j = 0; j < imageCount[i]; j++)
-                {
-                    if (i == size - 1 && j == imageCount[i] - 1)
-                        printf("(%i, %i)\n", set[i], images[i][j]);
-                    else
-                        printf("(%i, %i), ", set[i], images[i][j]);
-                }
-            }
-        }
-        printf("}\n");
-        printf("Relation Matrix for Relation R:\n");
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = 0; j < size; j++)
-                printf("%i ", matrix[i][j]);
-            printf("\n");
-        }
-        printf("\n");
-    }
-    else
-    {
-        printf("}\n\n");
-    }
-}
-
-
-bool Relation::isReflexive()
-{
-    if (size == 0)
-        return false;
-    for (int i = 0; i < size; i++)
-        if (!matrix[i][i])
-            return false;
-    return true;
-}
-
-
-bool Relation::isSymmetric()
-{
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            if (matrix[i][j] && !matrix[j][i])
-                return false;
-    return true;
-}
-
-
-bool Relation::isAntiSymmetric()
-{
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            if (matrix[i][j] && matrix[j][i] && i != j)
-                return false;
-    return true;
-}
-
-
-bool Relation::isTransitive()
-{
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            for (int k = 0; k < size; k++)
-                if (matrix[i][j] && matrix[j][k] && !matrix[i][k])
-                    return false;
-    return true;
+	int f=1;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(ar[i][j]==1 && ar[j][i]==1 && i!=j)
+			{
+				f=0;
+				break;
+			}
+		}
+		if(f==0)
+			break;
+	}
+	if(f==1)
+		return true;
+	else
+		return false;
 }
 
 int main()
 {
-    cout<<"PRACTICAL 4"<<endl;
-    cout<<endl;
-    Relation r;
-    r.display();
-
-    bool isEquivalent = r.isReflexive() &&
-                        r.isSymmetric() &&
-                        r.isTransitive();
-
-    bool isPartiallyOrdered = r.isReflexive() &&
-                              r.isAntiSymmetric() &&
-                              r.isTransitive();
-    printf("Relation R is %s\n",
-     	isEquivalent && isPartiallyOrdered ?
-         	"an equivalent as well as a partially-ordered relation"
-       	  : isEquivalent ? "an equivalent relation"
-       	  : isPartiallyOrdered ? "a partially-ordered relation"
-       	  : "neither an equivalent nor a partially-ordered relation"
-	);
-     return 0;
- }
+	relation r;
+	r.input();
+	r.display();
+	if(r.reflexive()&&r.symmetric()&&r.transitive())
+	    cout<<"The given relation is Equivalence";
+	else if(r.reflexive()&&r.antisymmetric()&&r.transitive())
+	    cout<<"The given relation is a partial order relation";
+	else
+	    cout<<"The relation is neither equivalence nor partial order relation";
+	return 0;
+}
